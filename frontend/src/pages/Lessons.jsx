2f182b006d921f79928100f09532ccfbcd36b08e
Louis MATHIS <114163976+Alphaeo@users.noreply.github.com>
2025-10-06 19:05:47 +0900

    Create Lessons.jsx

---
+++ frontend/src/pages/Lessons.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Lessons() {
+  return (
+    <div>
+      <h2 className="text-xl font-bold mb-4">Lessons Page</h2>
+      <p>Contenu des leçons ici…</p>
+    </div>
+  );
+}
