614086da09ffafb68b0007c8388f266ee5920f99
Louis MATHIS <114163976+Alphaeo@users.noreply.github.com>
2025-10-06 19:05:26 +0900

    Create Analyse.jsx

---
+++ frontend/src/pages/Analyse.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Analyse() {
+  return (
+    <div>
+      <h2 className="text-xl font-bold mb-4">Analyse Page</h2>
+      <p>Contenu d'analyse ici…</p>
+    </div>
+  );
+}
