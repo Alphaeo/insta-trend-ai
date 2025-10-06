281a8c89c1425196c68480ceb2a7992e91014f27
Louis MATHIS <114163976+Alphaeo@users.noreply.github.com>
2025-10-06 19:05:04 +0900

    Create Trend.jsx

---
+++ frontend/src/pages/Trend.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Feed from "../components/Feed";
+
+export default function Trend() {
+  return (
+    <div>
+      <h2 className="text-xl font-bold mb-4">Trend Page</h2>
+      <Feed />
+    </div>
+  );
+}
